0ab9a48893207996fc988bdaffeacfbb86ef1fb5
Adam Huang <adamhut@gmail.com>
2021-04-08 07:13:11 -0700

    wip ep 18

---
+++ tests/Feature/VoteShowPageTest.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Models\Idea;
+use App\Models\User;
+use App\Models\Vote;
+use App\Models\Status;
+use Livewire\Livewire;
+use App\Models\Category;
+use App\Http\Livewire\IdeaShow;
+use Database\Factories\VoteFactory;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class VoteShowPageTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function show_page_contain_idea_show_livewire_component()
+    {
+        $user = User::factory()->create();
+
+        $categoryOne = Category::factory()->create(['name' => 'category1']);
+
+        $statusOpen = Status::factory()->create(['name' => 'Open', 'classes' => 'bg-gray-200']);
+
+        $idea1 = Idea::factory()->create([
+            'user_id' => $user->id,
+            'title' => 'My first idea',
+            'category_id'   => $categoryOne->id,
+            'status_id'     =>  $statusOpen->id,
+            'description' => 'Description of my first idea',
+        ]);
+
+        $response = $this->get(route('idea.show',['idea'=>$idea1]));
+
+        $response->assertSeeLivewire('idea-show');
+
+    }
+
+
+    /** @test */
+    public function show_page_correctly_receives_votes_count()
+    {
+        $user = User::factory()->create();
+        $userB = User::factory()->create();
+
+        $categoryOne = Category::factory()->create(['name' => 'category1']);
+        $statusOpen = Status::factory()->create(['name' => 'Open', 'classes' => 'bg-gray-200']);
+
+        $idea1 = Idea::factory()->create([
+            'user_id' => $user->id,
+            'title' => 'My first idea',
+            'category_id'   => $categoryOne->id,
+            'status_id'     =>  $statusOpen->id,
+            'description' => 'Description of my first idea',
+        ]);
+
+        Vote::factory()->create([
+            'idea_id'=>$idea1->id,
+            'user_id' => $user->id,
+        ]);
+
+        Vote::factory()->create([
+            'idea_id' => $idea1->id,
+            'user_id' => $userB->id,
+        ]);
+
+
+        $this->get(route('idea.show', ['idea' => $idea1]))
+            ->assertViewHas('votesCount',2);
+    }
+
+
+    /** @test */
+    public function votes_count_show_correctly_on_show_page_livewire_component()
+    {
+        $user = User::factory()->create();
+        $userB = User::factory()->create();
+
+        $categoryOne = Category::factory()->create(['name' => 'category1']);
+        $statusOpen = Status::factory()->create(['name' => 'Open', 'classes' => 'bg-gray-200']);
+
+        $idea = Idea::factory()->create([
+            'user_id' => $user->id,
+            'title' => 'My first idea',
+            'category_id'   => $categoryOne->id,
+            'status_id'     =>  $statusOpen->id,
+            'description' => 'Description of my first idea',
+        ]);
+
+
+        Livewire::test(IdeaShow::class,[
+                'idea' => $idea,
+                'votesCount' => 5,
+            ])
+            ->assertSet('votesCount',5)
+            ->assertSeeHtml('<span class="text-xl leading-snug">5</span>')
+            ->assertSeeHtml(' <div class="text-sm font-bold leading-none">5</div>');
+        
+        
+    }
+
+}
