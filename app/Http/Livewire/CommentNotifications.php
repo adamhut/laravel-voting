18c9257907f93bd7721cbb4c3a9d11e334380e2a
adamhut <adamhut@gmail.com>
2021-07-09 21:09:38 -0700

    wip  ep 61

--- app/Http/Livewire/CommentNotifications.php
+++ app/Http/Livewire/CommentNotifications.php
@@ -6,22 +6,43 @@ use Livewire\Component;
 
 class CommentNotifications extends Component
 {
+    const NOTIFICATION_COUNT_THRESHOLD = 10;
 
     public $notifications  ;
 
+    public $notificationCount;
 
+    public $isLoading = true;
 
     protected $listeners =['getNotifications'];
 
     public function mount()
     {
-
         $this->notifications = collect();
+        $this->isLoading = true;
+        $this->getNotificationCount();
     }
 
     public function getNotifications()
     {
-        $this->notifications =auth()->user()->unreadNotifications;
+
+        $this->notifications =auth()
+            ->user()
+            ->unreadNotifications()
+            ->take(self::NOTIFICATION_COUNT_THRESHOLD)
+            ->get();
+
+        $this->isLoading = false;
+    }
+
+    public function getNotificationCount()
+    {
+        $this->notificationCount = auth()
+            ->user()
+            ->unreadNotifications()
+            ->count();
+
+        $this->notificationCount =  $this->notificationCount > self::NOTIFICATION_COUNT_THRESHOLD ? self::NOTIFICATION_COUNT_THRESHOLD.'+': $this->notificationCount;
     }
 
 
--- resources/views/livewire/ideas-index.blade.php
+++ resources/views/livewire/ideas-index.blade.php
@@ -5,19 +5,19 @@
                 <option value="All Categories">All Categories</option>
                 @foreach($categories as $category)
                     <option  value="{{$category->name}}">{{$category->name}}</option>
-                @endforeach                
+                @endforeach
             </select>
         </div>
         <div class="w-full md:w-1/3">
-            <select 
-                wire:model="filter" 
-                name="other_filters" 
-                id="other_filters" 
+            <select
+                wire:model="filter"
+                name="other_filters"
+                id="other_filters"
                 class="w-full rounded-xl py-2 border-none shadow-sm"
             >
                 <option value="No Filter">No Filter</option>
                 <option value="Top Voted">Top Voted</option>
-                <option value="My Ideas">My Ideas</option> 
+                <option value="My Ideas">My Ideas</option>
                 @admin
                     <option value="Spam Idea">Spam Idea</option>
                     <option value="Spam Comments">Spam Comments</option>
@@ -25,9 +25,9 @@
             </select>
         </div>
         <div class="w-full md:w-2/3 relative">
-            <input 
+            <input
                 wire:model="search"
-                type="search" 
+                type="search"
                 placeholder="Find an Ideas"
                 class="w-full rounded-xl bg-white border-none px-4 py-2 pl-8 placeholder-gray-900">
             <div class="absolute top-0 flex items-center h-full ml-2">
@@ -37,29 +37,28 @@
                         d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
                 </svg>
             </div>
-    
+
         </div>
     </div>
     <!--End Filters-->
-    
+
     <div class="my-6 ideas-container space-y-6 ">
         @forelse ($ideas as $idea)
-            <livewire:idea-index 
-                :key="$idea->id" 
-                :idea="$idea" 
-                :votesCount="$idea->votes_count" 
+            <livewire:idea-index
+                :key="$idea->id"
+                :idea="$idea"
+                :votesCount="$idea->votes_count"
             />
         @empty
             <div class="mx-auto w-70 mt-12 ">
-                <img 
-                    src="{{ asset('images/no-ideas.svg') }}" 
-                    alt="No Ideas" 
-                    class="mx-auto" 
+                <img
+                    src="{{ asset('images/no-ideas.svg') }}"
+                    alt="No Ideas"
+                    class="mx-auto"
                     style="mix-blend-mode: luminosity"
                 >
                 <div class="text-gray-400 text-center font-bold mt-6">No ideas were found...</div>
-                
-            </div>    
+            </div>
 
         @endforelse
         <div class="my-8">
